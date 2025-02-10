e7a4fa3a7ff1c097a0099488446ae597e203cc12
otapogit <carlosotapo@gmail.com>
2025-02-10 18:04:51 +0100

    spawner basico

---
+++ Assets/Scripts/Photon/Spawner.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using Fusion;
+using Fusion.Sockets;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
+{
+
+    private NetworkRunner _runner; 
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) {}
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) {}
+    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) {}
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) {}
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) {}
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key,
+    ArraySegment<byte> data) {}
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float
+    progress) {}
+
+
+    
+    async void StartGame(GameMode mode) {
+    // Create the Fusion runner and let it know that we will be providing user input
+        _runner = gameObject.AddComponent<NetworkRunner>();
+        _runner.ProvideInput = true;
+        // Create the NetworkSceneInfo from the current scene
+        SceneRef scene = SceneRef.FromIndex(SceneManager.GetActiveScene().buildIndex);
+        NetworkSceneInfo sceneInfo = new NetworkSceneInfo();
+        if (scene.IsValid) 
+            sceneInfo.AddSceneRef(scene, LoadSceneMode.Additive);
+        // Start or join (depends on gamemode) a session with a specific name
+            await _runner.StartGame(new StartGameArgs()
+            {
+                GameMode = mode,
+                SessionName = "TestRoom",
+                Scene = scene,
+                SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+            });
+    }
+
+    private void OnGUI()
+    {
+        if (_runner == null) {
+            if (GUI.Button(new Rect(0, 0, 200, 40), "Host"))
+                StartGame(GameMode.Host);
+            if (GUI.Button(new Rect(0, 40, 200, 40), "Join"))
+                StartGame(GameMode.Client);
+        }
+    }
+
+}
